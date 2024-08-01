5b4a145d6ea1c8461850556cc506c0f7769a7ac7
aivaneezy1 <aivaneezydakii@gmail.com>
2024-08-01 21:38:14 +0200

    fixed vercel error pt2

--- app/pages/_document.jsx
+++ app/pages/_document.jsx
@@ -1,17 +1,20 @@
 // pages/_document.js
 import Document, { Html, Head, Main, NextScript } from 'next/document';
+import Script from 'next/script';
 
 class MyDocument extends Document {
   render() {
     return (
       <Html>
         <Head>
-          <script
-            type="text/javascript"
+          <Script
             src="https://cdn.jsdelivr.net/npm/@emailjs/browser@4/dist/email.min.js"
-          ></script>
-          <script
-            type="text/javascript"
+            strategy="lazyOnload" // or "lazyOnload" if you don't need it immediately
+     
+          />
+          <Script
+            id="emailjs-init"
+            strategy="lazyOnload" // or "lazyOnload" if you don't need it immediately
             dangerouslySetInnerHTML={{
               __html: `
                 (function(){
